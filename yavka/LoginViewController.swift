a3b7f4305f7f5cb152c0625e9a6da5c00f436411
greycrane <gcrane@bk.ru>
2020-07-29 17:08:06 +0200

    homework 1 done

--- yavka/LoginViewController.swift
+++ yavka/LoginViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        guard loginField.text != "", loginField.text != nil else {
+            print("Имя пользователя не может быть пустым.")
+            return
+        }
+        guard passwordField.text != "", passwordField.text != nil else {
+            print("Пароль не может быть пустым.")
+            return
+        }
+        
+        if loginField.text == "admin", passwordField.text == "12345" {
+            print("Вы успешно авторизовались!")
+        } else {
+            print("Неверное имя пользователя и/или пароль.")
+        }
+    }
+    
+    @objc func keyboardWillBeShown(notification: Notification) {
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 20.0, right: 0)
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+    @objc func hideKeyboard() {
+        scrollView.endEditing(true)
+    }
+    
+    private func showSignInError() {
+        let alert = UIAlertController(title: "Error", message: "Login and/or password is incorrect", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
-
